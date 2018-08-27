e5b742bded197d05408554acaa7700d6650a2e10
B533P-05\533 <test@hotmail.com>
2018-08-27 17:08:12 -0400

    Jeu après cours 1

---
+++ DemoCours3/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        Vector3 adjustedMovement = transform.TransformDirection(Vector3.forward);
+        rb.AddForce(adjustedMovement * 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
